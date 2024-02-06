e87d519289754a7bb60be3c4636fdf6695d5a122
Molkang <mitier26@gmail.com>
2024-02-06 19:17:53 +0900

    test commit

---
+++ javascript/test.js
@@ -0,0 +1,20 @@
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.table(animals.slice(1, 4));
+console.table(animals);
+
+// console.log(animals);
+// console.log(animals.splice(-2, 0));
+// console.log(animals[-2]);
+// console.table(animals.slice(-2, 1));
+// console.log(animals.length - 1);
+// console.log(animals[animals.length - 1]);
+// console.table(animals.slice(animals.length - 2, 1));
+
+// let fruits = ['apple', 'banana', 'orange'];
+
+// console.log(fruits);
+// console.log(fruits[1]);
+
+// fruits.pop();
+// console.log(fruits);
