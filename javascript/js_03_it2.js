898a3c0559ca5b275d58a7d5f56ab03e1943c578
Molkang <mitier26@gmail.com>
2024-02-07 19:16:21 +0900

    samhang

--- javascript/js_03_it2.js
+++ javascript/js_03_it2.js
@@ -7,7 +7,7 @@ if (inputNum == 0) {
     console.log('입력하신 숫자', inputNum, '은 양수 입니다.');
 }
 
-let inputScore = 88;
+let inputScore = 45;
 if (inputScore >= 90) {
     console.log('A');
 } else if (inputScore >= 80) {
@@ -19,6 +19,24 @@ if (inputScore >= 90) {
 } else {
     console.log('F');
 }
+
+console.log('이것 되는 것?');
+
+console.log(
+    inputScore > 100
+        ? '100이하의 값을 넣어 주세요'
+        : inputScore >= 90
+        ? 'A'
+        : inputScore >= 80
+        ? 'B'
+        : inputScore >= 70
+        ? 'C'
+        : inputScore >= 60
+        ? 'D'
+        : inputScore < 0
+        ? '0 이상의 값을 주세요'
+        : 'F'
+);
 
 let skills = ['HTML', 'CSS', 'Javascript', 'React'];
 
