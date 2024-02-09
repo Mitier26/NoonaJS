894f7e42cddf8193c2883bf4c725daefad4bde0e
Molkang <mitier26@gmail.com>
2024-02-09 23:12:05 +0900

    test

--- javascript/test.js
+++ javascript/test.js
@@ -1,7 +1,7 @@
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
 
-console.table(animals.slice(1, 4));
-console.table(animals);
+// console.table(animals.slice(1, 4));
+// console.table(animals);
 
 // console.log(animals);
 // console.log(animals.splice(-2, 0));
@@ -19,16 +19,28 @@ console.table(animals);
 // fruits.pop();
 // console.log(fruits);
 
-let l = 11;
-for (k = 2; k < l; k++) {
-    console.log(k);
-    console.log(l);
-    console.log(l % k);
-    if (l % k === 0) {
-        console.log('false');
-    } else if (l === 1) {
-        console.log('false');
+// let l = 11;
+// for (k = 2; k < l; k++) {
+//     console.log(k);
+//     console.log(l);
+//     console.log(l % k);
+//     if (l % k === 0) {
+//         console.log('false');
+//     } else if (l === 1) {
+//         console.log('false');
+//     } else {
+//         console.log('true');
+//     }
+// }
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 10 === 3 || i % 10 === 6 || i % 10 === 9) {
+        console.log('박수');
+        if (i >= 30 && i <= 39) {
+            console.log('박수');
+        } else {
+        }
     } else {
-        console.log('true');
+        console.log(i);
     }
 }
